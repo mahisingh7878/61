7d292fe561b8468aedab9049e09c1deab1ab1e52
mahisingh4415 <mahisinghadityasingh1232@gmail.com>
2025-01-24 21:41:45 +0530

    1

---
+++ actvity1.py
@@ -0,0 +1,10 @@
+num1=int(input("enter a number "))
+num2=int(input("enter  another number "))
+
+
+print("Bitwise OR of {} and {} is {}".format(num1,num2,num1|num2))
+                                            
+print("Bitwise AND of {} and {} is {}".format(num1,num2,num1&num2))
+print("Bitwise XOR of {} and {} is {}".format(num1,num2,num1^num2))
+print("Bitwise NOT of {} and {} is {}".format(num1,num2,~num1,~num2))
+
